79576a0b378df3f3caf270711134196904989b35
Orly kanyinda <107365279+orlykanyinda2@users.noreply.github.com>
2023-02-02 15:44:18 +0300

    Update code-2.py

--- code-2.py
+++ code-2.py
@@ -1,67 +1,21 @@
-from pytube import YouTube
-import re
+import youtube_dl
 
-# Define a regular expression pattern for a valid YouTube video URL
-youtube_url_pattern = re.compile(r'https?://(www\.)?youtube\.com/watch\?v=[A-Za-z0-9_-]{11}')
+def reset_ydl():
+    global ydl
+    ydl = youtube_dl.YoutubeDL({'format': 'bestaudio/best'})
 
-# Get the URL of the video from the user
-url = input("Enter the URL of the video: ")
+reset_ydl()
 
-# Check if the URL is a valid YouTube video URL
-if not youtube_url_pattern.match(url):
-    print("Invalid URL. Please enter a valid YouTube video URL.")
-    exit()
+def download_video(url):
+    try:
+        ydl.download([url])
+    except Exception as e:
+        print(e)
+        reset_ydl()
 
-try:
-    # Get the video from YouTube
-    yt = YouTube(url)
-
-    # Get the available file types for the video
-    video_formats = yt.filter('mp4')
-    audio_formats = yt.filter('mp4')
-
-    # Get the available video qualities
-    video_qualities = []
-    for video in video_formats:
-        if video.resolution not in video_qualities:
-            video_qualities.append(video.resolution)
-
-    # Prompt user to select the file type
-    print("Select the file type:")
-    print("1. Video")
-    print("2. Audio")
-    selected_type = input("Enter the option number: ")
-
-    # Check if the selected type is a valid number
-    if not selected_type.isdigit() or int(selected_type) not in range(1, 3):
-        print("Invalid option. Please enter a valid option number.")
-        exit()
-
-    # Prompt user to select the video quality
-    print("Select the video quality:")
-    for i, quality in enumerate(video_qualities):
-        print(f"{i+1}. {quality}")
-    selected_quality = input("Enter the option number: ")
-
-    # Check if the selected quality is a valid number
-    if not selected_quality.isdigit() or int(selected_quality) not in range(1, len(video_qualities)+1):
-        print("Invalid option. Please enter a valid option number.")
-        exit()
-
-    # Get the selected video quality
-    selected_quality = video_qualities[int(selected_quality)-1]
-
-    if selected_type == '1':
-        # Get the selected file type
-        selected_type = video_formats.filter(resolution=selected_quality).first()
-        # Download the video
-        selected_type.download(filepath=f'{yt.title}.{selected_type.extension}')
-        print(f'{yt.title}.{selected_type.extension} downloaded!')
-    else:
-        # Get the selected file type
-        selected_type = audio_formats.first()
-        # Download the audio
-        selected_type.download(filepath=f'{yt.title}.{selected_type.extension}')
-        print(f'{yt.title}.{selected_type.extension} downloaded!')
-except Exception as e:
-    print(f"An error occurred: {e}")
+while True:
+    url = input("Enter the YouTube video URL: ")
+    if url == "reset":
+        reset_ydl()
+        continue
+    download_video(url)
